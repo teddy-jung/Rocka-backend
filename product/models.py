0ba1d264c7a2d0d8d682d47c76a52c51130fe568
TeddyJung <teddyjung@TeddyJungui-MacBookPro.local>
2020-06-23 20:22:00 +0900

    [Add] Product 앱 생성, Product Model 작성 - product 앱 생성 - product 의 models.py 작성 - 다른 앱과 연결되는 foreignkey 주석처리 하였음

---
+++ product/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'categories'
+
+class Product(models.Model):
+    name                    = models.CharField(max_length=50)
+    simple_description      = models.CharField(max_length=100)
+    price                   = models.DecimalField(max_digits=10, decimal_places=2)
+    #shipping_information    = models.ForeignKey('ShippingInformation', on_delete=models.SET_NULL, null=True)
+    basic_information       = models.ForeignKey('BasicInformation', on_delete=models.SET_NULL, null=True)
+    weight                  = models.DecimalField(max_digits=10, decimal_places=2)
+    usage                   = models.CharField(max_length=50)
+    kfda_audit              = models.CharField(max_length=100)
+    caution                 = models.CharField(max_length=1000)
+    color                   = models.ForeignKey('Color', on_delete=models.SET_NULL, null=True)
+    is_soldout              = models.BooleanField(default=False)
+    manufacturer            = models.ForeignKey('Manufacturer', on_delete=models.SET_NULL, null=True)
+    outer_back_image_url    = models.URLField(max_length=2000, null=True)
+    outer_front_image_url   = models.URLField(max_length=2000, null=True)
+    inner_image_url         = models.URLField(max_length=2000, null=True)
+
+    class Meta:
+        db_table = 'products'
+
+class CategoryProduct(models.Model):
+    category    = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    product     = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = 'categories_products'
+
+class Color(models.Model):
+    name            = models.CharField(max_length=50)
+    color_url       = models.URLField(max_length=2000, null=True)
+    ingredient_info = models.CharField(max_length=2000, null=True)
+    stock_quantity  = models.IntegerField(default=1000)
+
+    class Meta:
+        db_table = 'colors'
+
+class BasicInformation(models.Model):
+    main_spec           = models.CharField(max_length=50)
+    counseling_info     = models.CharField(max_length=500)
+    return_policy       = models.CharField(max_length=2000)
+    expiration_date     = models.CharField(max_length=50)
+    country             = models.CharField(max_length=50)
+    quality_assurance   = models.CharField(max_length=200)
+    vendor              = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'basicinformation'
+
+class Manufacturer(models.Model):
+    name    = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'manufacturers'
